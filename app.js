9b903a3f7da7bdeafb9bd0afdbfe0c322694e3d7
Sumit <sumit98765tiwariji@gmail.com>
2026-05-03 11:40:50 +0530

    Ensure app uses Railway dynamic port

--- app.js
+++ app.js
@@ -75,6 +75,7 @@ app.use(function (err, req, res, next) {
 });
 
 
-app.listen(3000, () => {
-  console.log("server started on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
 });
